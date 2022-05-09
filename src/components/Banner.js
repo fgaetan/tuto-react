3957996404cadefa377b8cf3fac72045edea09e0
fgaetan <frion.gaetan@gmail.com>
2022-05-09 16:43:00 +0200

    09 05 2022 first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import "../styles/Banner.css";
+import logo from "../assets/logo.png";
+
+function Banner() {
+        return (
+                <div className="lmj-banner">
+                        <img
+                                src={logo}
+                                alt="Lamaison jungle"
+                                className="lmj-logo"
+                        />
+                        <h1>La maison jungle</h1>
+                </div>
+        );
+}
+
+export default Banner;
